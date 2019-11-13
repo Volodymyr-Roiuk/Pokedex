a32bd1609aaf4ac06c765b6a3fe3df59ef22e08f
Volodymyr_Roiuk <heppivova@gmail.com>
2019-11-13 15:12:48 +0300

    Updates

---
+++ precache-manifest.deff7a5e956449acbbcf14d3704f277c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3742f9a5a4cbf08689bcfa28b3eb659d",
+    "url": "/Pokedex/index.html"
+  },
+  {
+    "revision": "87562ab268cf89f590b2",
+    "url": "/Pokedex/static/css/main.65938976.chunk.css"
+  },
+  {
+    "revision": "ea7322c7fb23015396bd",
+    "url": "/Pokedex/static/js/2.54e923d0.chunk.js"
+  },
+  {
+    "revision": "87562ab268cf89f590b2",
+    "url": "/Pokedex/static/js/main.f9de95e1.chunk.js"
+  },
+  {
+    "revision": "34dcb7c62138091398c4",
+    "url": "/Pokedex/static/js/runtime-main.ba04c60a.js"
+  }
+]);
